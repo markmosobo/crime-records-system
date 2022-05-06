078a4ad010f89f8bc14019a7ebfc76ba1bba42c3
unknown <mmosobo@gmail.com>
2022-05-06 12:21:40 +0300

    first commit

---
+++ admin/dbconnect2.php
@@ -0,0 +1,5 @@
+<?php  
+$dbcon = mysqli_connect ("localhost", "root", "", "crimerecords");
+mysqli_set_charset($dbcon, 'utf8'); 
+
+?>
---
+++ admin/header.php
@@ -0,0 +1,19 @@
+ <!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kenya Police Service </title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap-theme.min.css">
+
+     <!-- Custom CSS -->
+    <link href="../assets/css/simple-sidebar.css" rel="stylesheet">
+    <link href="../assets/css/dataTables.bootstrap.min.css" rel="stylesheet">
+
+  </head>
+<body>
+
